6fe57acd2b32464696e6094a4ffd95fcda77d68e
heavensbee <heavensbee001@gmail.com>
2023-05-12 15:45:00 +0200

    Some css fixes

--- packages/ui/src/elements/PopoverOnHover/PopoverOnHover.tsx
+++ packages/ui/src/elements/PopoverOnHover/PopoverOnHover.tsx
@@ -61,7 +61,7 @@ export const PopoverOnHover: FC<PopoverOnHoverProps> = ({
       </div>
 
       <div
-        className={`p-4 w-${
+        className={`p-2 w-${
           size ? (size === "sm" ? "36" : size === "md" ? "48" : "80") : "full"
         } rounded-md border border-gray-200 bg-white shadow-lg`}
         onMouseEnter={open}
