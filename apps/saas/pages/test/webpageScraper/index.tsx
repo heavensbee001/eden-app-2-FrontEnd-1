3de95292172172573e7ceb56d1f36c7ec01db6ee
Sergey Belyaev <79952005+sbelka-1703@users.noreply.github.com>
2023-05-24 09:15:46 -0700

    Initial api call created for WEBPAGE_TO_MEMORY

--- apps/saas/pages/test/webpageScraper/index.tsx
+++ apps/saas/pages/test/webpageScraper/index.tsx
@@ -1,5 +1,22 @@
+/* API Call  */
+// import { gql, useMutation } from "@apollo/client";
+/* API Call  */
+// import { UserContext } from "@eden/package-context";
 import { Button, TextArea } from "@eden/package-ui";
+/* API Call, import useContext */
 import { ChangeEvent, FormEvent, useState } from "react";
+
+/* API Call  */
+
+// const { currentUser } = useContext(UserContext);
+
+// export const WEBPAGE_TO_MEMORY = gql`
+//   mutation ($fields: WebsiteToMemoryCompanyInput!) {
+//     WebsiteToMemoryCompany(fields: $fields) {
+//       result
+//     }
+//   }
+// `;
 
 const LinkedInScraper = () => {
   const [webpageLink, setWebpageLink] = useState("");
@@ -8,6 +25,15 @@ const LinkedInScraper = () => {
   const [webPageText, setWebPageText] = useState("");
   const [scraping, setScraping] = useState<boolean>(false);
   const [error, setError] = useState<string | null>(null);
+
+  /* API Call  */
+
+  // const [WebpageToMemory] = useMutation(WEBPAGE_TO_MEMORY, {
+  //   onCompleted({ WebpageToMemory }) {
+  //     console.log("Memory Recorded");
+  //     console.log(WebpageToMemory);
+  //   },
+  // });
 
   const handleWebpageLinkChange = (e: ChangeEvent<HTMLInputElement>) => {
     setWebpageLink(e.target.value);
@@ -51,6 +77,14 @@ const LinkedInScraper = () => {
 
       const { textResponse } = await response.json();
 
+      /* API Call  */
+
+      // WebpageToMemory({
+      //   variables: {
+      //     fields: { message: textResponse, userID: currentUser?._id },
+      //   },
+      // });
+
       setWebPageText(textResponse);
     } catch (error) {
       setError(
@@ -68,6 +102,14 @@ const LinkedInScraper = () => {
   const handleTextSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     console.log("pastedText", pastedText);
+
+    /* API Call  */
+
+    // WebpageToMemory({
+    //   variables: {
+    //     fields: { message: pastedText, userID: currentUser?._id },
+    //   },
+    // });
     setPastedText("");
   };
 
