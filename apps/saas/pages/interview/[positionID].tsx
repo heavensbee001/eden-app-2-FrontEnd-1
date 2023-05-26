66bb7c9150ba426812aa9a9b16606682cbf83fde
Sergey Belyaev <79952005+sbelka-1703@users.noreply.github.com>
2023-05-25 18:40:11 -0700

    First page of interview is done

--- apps/saas/pages/interview/[positionID].tsx
+++ apps/saas/pages/interview/[positionID].tsx
@@ -28,6 +28,7 @@ const HomePage: NextPageWithLayout = () => {
   const [interviewEnded, setInterviewEnded] = useState(false);
   const [cvEnded, setCvEnded] = useState<Boolean>(false);
   const [progress, setProgress] = useState<number>(0);
+  const [titleRole, setTitleRole] = useState(null);
 
   console.log("cvEnded = ", cvEnded);
   const {
@@ -71,6 +72,12 @@ const HomePage: NextPageWithLayout = () => {
     }
   };
 
+  const handleDataFromCVUploadGPT = (data: any) => {
+    const role = data.saveCVtoUser.titleRole;
+
+    setTitleRole(role);
+  };
+
   return (
     <>
       <Head>
@@ -108,18 +115,57 @@ const HomePage: NextPageWithLayout = () => {
                   <h3 className="mb-8 text-center text-lg font-medium">
                     Hey {currentUser?.discordName}!
                   </h3>
-                  <p className="mb-8 text-center">
-                    In order for me to be able to ask relevant questions,
+                  <p className="mb-4 text-center">
+                    Congratulations! You&apos;ve Been Selected To
                     <br />
-                    please upload your CV first.
+                    complete A First interview with <strong>Tesla</strong>
                     <br />
-                    <br />
-                    The AI Magic 🪄 takes exactly 30 seconds
+                    For the {titleRole ? (
+                      <strong>{titleRole}</strong>
+                    ) : null}{" "}
+                    Role
                   </p>
                   <CVUploadGPT
+                    onDataReceived={handleDataFromCVUploadGPT}
                     handleEnd={handleCvEnd}
                     positionID={positionID}
                   />
+
+                  <p className="mt-4  flex ">
+                    AI
+                    <span className="mx-[4px] flex items-center">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="gold"
+                        viewBox="0 0 24 24"
+                        strokeWidth="1.5"
+                        stroke="currentColor"
+                        className="h-[12px] w-[12px]"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z"
+                        />
+                      </svg>
+                      Magic
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="gold"
+                        viewBox="0 0 24 24"
+                        strokeWidth="1.5"
+                        stroke="currentColor"
+                        className="h-[12px] w-[12px]"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z"
+                        />
+                      </svg>
+                    </span>
+                    Takes 30 Seconds
+                  </p>
                 </section>
               </WizardStep>
               <WizardStep label={"welcome"}>
