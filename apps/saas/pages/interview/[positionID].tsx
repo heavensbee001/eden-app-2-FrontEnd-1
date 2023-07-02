0af35c81d0afdae737f233105f9a62f38fafc1dc
heavensbee <heavensbee001@gmail.com>
2023-07-02 17:07:38 +0200

    Interview step 1 structure

--- apps/saas/pages/interview/[positionID].tsx
+++ apps/saas/pages/interview/[positionID].tsx
@@ -24,6 +24,7 @@ import {
   useEffect,
   useState,
 } from "react";
+import { AiOutlineLock } from "react-icons/ai";
 
 // import { rawDataPersonProject } from "../../utils/data/rawDataPersonProject";
 import type { NextPageWithLayout } from "../_app";
@@ -113,12 +114,9 @@ const HomePage: NextPageWithLayout = () => {
         />
       </Head>
       <SEO />
-      <Card
-        className="mx-auto mt-3 h-[88vh] w-full max-w-5xl overflow-y-scroll bg-white"
-        shadow
-      >
+      <Card className="mx-auto mt-3 h-[88vh] w-full max-w-5xl overflow-y-scroll rounded-none pt-4">
         {currentUser && (
-          <div className="h-full w-full p-8">
+          <div className="h-full w-full">
             <div className="absolute left-0 top-0 w-full">
               <ProgressBarGeneric progress={progress} />
             </div>
@@ -303,59 +301,47 @@ const UploadCVContainer = ({
   };
 
   return (
-    <section className="flex h-full flex-col items-center justify-center">
-      <h3 className="mb-8 text-center text-lg font-medium">
-        Hey {currentUser?.discordName}!
-      </h3>
-      <p className="mb-4 text-center">
-        Congratulations! You&apos;ve Been Selected To
-        <br />
-        complete A First interview with <strong>Tesla</strong>
-        <br />
-        For the {titleRole ? <strong>{titleRole}</strong> : null} Role
-      </p>
-      <CVUploadGPT
-        onDataReceived={handleDataFromCVUploadGPT}
-        handleEnd={handleCvEnd}
-        positionID={positionID}
-      />
-
-      <p className="mt-4  flex ">
-        AI
-        <span className="mx-[4px] flex items-center">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="gold"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            className="h-[12px] w-[12px]"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z"
-            />
-          </svg>
-          Magic
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="gold"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            className="h-[12px] w-[12px]"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z"
-            />
-          </svg>
-        </span>
-        Takes 30 Seconds
-      </p>
-    </section>
+    <div className="">
+      <section className="mb-2 flex h-[25vh] w-full flex-col items-center justify-center rounded-md border border-gray-300 bg-white p-4">
+        <h3 className="mb-4 text-center text-lg font-medium">
+          Hey {currentUser?.discordName}!
+        </h3>
+        <p className="mb-4 text-center">
+          Congratulations! You&apos;ve Been Selected To
+          <br />
+          complete A First interview with <strong>Tesla</strong>
+          <br />
+          For the {titleRole ? <strong>{titleRole}</strong> : null} Role
+        </p>
+        <CVUploadGPT
+          onDataReceived={handleDataFromCVUploadGPT}
+          handleEnd={handleCvEnd}
+          positionID={positionID}
+        />
+      </section>
+      <section className="grid h-[50vh] grid-cols-3 gap-2">
+        <div className="col-span-1 h-full rounded-md border border-gray-300 bg-white p-4"></div>
+        <div className="col-span-1 h-full rounded-md border border-gray-300 bg-white p-4"></div>
+        <div className="col-span-1 h-full rounded-md border border-gray-300 bg-white p-4">
+          <h3 className="text-center text-lg font-semibold">
+            You x {"[Company name]"}
+          </h3>
+          <p className="mb-8 text-center text-gray-500">
+            <AiOutlineLock className="mr-2 inline-block" />
+            Upload your CV to unlock it
+          </p>
+          <ul className="list-disc pl-4">
+            <li className="mb-2">Probability of landing this job</li>
+            <li className="mb-2">What to improve to maximeze youre chances</li>
+            <li className="mb-2">
+              Ask specific questions about company, opportunity & culture in
+              real time chat
+            </li>
+            <li className="mb-2">Salary range + equity benefits</li>
+          </ul>
+        </div>
+      </section>
+    </div>
   );
 };
 
