534aaed465b28ca0a74fea2a458f63feabca9921
Sergey Belyaev <79952005+sbelka-1703@users.noreply.github.com>
2023-07-03 11:07:58 -0700

    init letter storybook

---
+++ packages/ui/src/components/EdenAiLetter/EdenAiLetter.tsx
@@ -0,0 +1,8 @@
+import { useMutation, useQuery } from "@apollo/client";
+import { UserContext } from "@eden/package-context";
+// import dynamic from "next/dynamic";
+import React, { useContext, useEffect, useState } from "react";
+
+export const EdenAiLetter = ({}) => {
+  return <>HI</>;
+};
