09bf43ef4006ceb7fe5e44253b2fe545dbf15867
heavensbee <heavensbee001@gmail.com>
2023-07-18 19:28:25 +0200

    Add position button working + Company context improvement

--- packages/context/src/CompanyContext/CompanyContext.tsx
+++ packages/context/src/CompanyContext/CompanyContext.tsx
@@ -3,8 +3,12 @@ import { createContext } from "react";
 
 export interface CompanyContextType {
   company: Company | undefined;
+  getCompanyFunc: () => void;
 }
 
 export const CompanyContext = createContext<CompanyContextType>({
   company: undefined,
+  getCompanyFunc: () => {
+    console.log("getCompanyFunc undefined");
+  },
 });
