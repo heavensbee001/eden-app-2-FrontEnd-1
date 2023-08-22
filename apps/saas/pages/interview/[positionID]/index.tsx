7a50edb9280f76ddc251a48a17854aed41195bc1
heavensbee <heavensbee001@gmail.com>
2023-08-22 15:32:16 +0200

    Change match text

--- apps/saas/pages/interview/[positionID]/index.tsx
+++ apps/saas/pages/interview/[positionID]/index.tsx
@@ -474,17 +474,16 @@ const ApplicationStepContainer = ({
   const today = new Date();
 
   const getMatchText = () => {
-    // switch
     if (content.matchPercentage! >= 80) {
-      return "Great fit";
+      return "Very High";
     } else if (content.matchPercentage! >= 60) {
-      return "Ok fit";
+      return "High";
     } else if (content.matchPercentage! >= 40) {
-      return "Not sure";
+      return "Average";
     } else if (content.matchPercentage! >= 20) {
-      return "Probably not a fit";
+      return "Low";
     } else if (content.matchPercentage! < 20) {
-      return "Definitely not a fit";
+      return "Very Low";
     } else {
       return "";
     }
