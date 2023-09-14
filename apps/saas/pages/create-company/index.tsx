617f0866910e29e72d5dd30505fc239cfb02f5cb
heavensbee <heavensbee001@gmail.com>
2023-09-14 12:54:23 +0200

    Improve create company page

---
+++ apps/saas/pages/create-company/index.tsx
@@ -0,0 +1,131 @@
+import { gql, useMutation } from "@apollo/client";
+import {
+  AppUserLayout,
+  Button,
+  EdenAiProcessingModal,
+  SEO,
+} from "@eden/package-ui";
+import { useRouter } from "next/router";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+
+// @ts-ignore
+import type NextPageWithLayout from "../../_app";
+
+type FormData = {
+  companyName: string;
+  companyAbbreviation: string;
+  companyDescription: string;
+};
+
+const UPDATE_COMPANY = gql`
+  mutation ($fields: updateCompanyInput!) {
+    updateCompany(fields: $fields) {
+      name
+      type
+      slug
+      description
+    }
+  }
+`;
+const CreateCompany: NextPageWithLayout = () => {
+  const [formData, setFormData] = useState<FormData | null>(null);
+  const [submitting, setSubmitting] = useState<boolean>(false);
+  const { register, handleSubmit } = useForm();
+
+  const router = useRouter();
+
+  const [updateCompany] = useMutation(UPDATE_COMPANY, {
+    // eslint-disable-next-line no-unused-vars
+    onCompleted({ data }) {
+      // console.log("data from onComplete", data);
+      if (formData) {
+        router.push(`/${formData.companyAbbreviation}/dashboard`);
+      }
+    },
+    onError() {
+      setSubmitting(false);
+    },
+  });
+
+  const submitHandler = (data: any) => {
+    setFormData(data);
+    // console.log("data from handler:", data);
+    setSubmitting(true);
+    updateCompany({
+      variables: {
+        fields: {
+          name: data.companyName,
+          slug: data.companyAbbreviation,
+          type: "COMPANY",
+          description: data.companyDescription,
+        },
+      },
+    });
+  };
+
+  return (
+    <>
+      <SEO />
+      <div className="h-screen w-full flex items-center justify-center">
+        <form
+          className="w-full max-w-2xl"
+          onSubmit={handleSubmit(submitHandler)}
+        >
+          <section className="mb-4 inline-block w-full space-y-6 p-4 pr-12">
+            <div className="space-y-1">
+              <p className="text-xs">Company Name</p>
+              <div className="border-EdenGray-100 flex w-full items-center rounded-md border bg-white text-xs">
+                <input
+                  type="text"
+                  id="Name"
+                  className="h-[34px] w-full bg-transparent p-2"
+                  required
+                  {...register("companyName")}
+                />
+              </div>
+            </div>
+
+            <div className="space-y-1">
+              <p className=" text-xs">Company Abbreviation</p>
+              <div className="border-EdenGray-100 flex w-full items-center rounded-md border bg-white text-xs">
+                <input
+                  type="text"
+                  id="Abbreviation"
+                  className="h-[34px] w-full bg-transparent p-2"
+                  required
+                  {...register("companyAbbreviation")}
+                />
+              </div>
+            </div>
+
+            <div className="space-y-1">
+              <p className="text-xs">Description of the Company</p>
+              <div>
+                <textarea
+                  className="border-EdenGray-100 w-full border p-2 text-xs"
+                  rows={6}
+                  id="Description"
+                  required
+                  {...register("companyDescription")}
+                />
+              </div>
+            </div>
+
+            <Button type="submit" variant="secondary" className="block mx-auto">
+              Submit
+            </Button>
+          </section>
+        </form>
+        <EdenAiProcessingModal
+          title="Creating your company"
+          open={submitting}
+        />
+      </div>
+    </>
+  );
+};
+
+CreateCompany.getLayout = (page: any) => <AppUserLayout>{page}</AppUserLayout>;
+
+export default CreateCompany;
