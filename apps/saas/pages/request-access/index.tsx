5ad7150ebeb15246d73095fe78a38dc3dc5f7282
Sergey Belyaev <79952005+sbelka-1703@users.noreply.github.com>
2023-09-25 15:16:32 +0200

    page created

--- apps/saas/pages/request-access/index.tsx
+++ apps/saas/pages/request-access/index.tsx
@@ -1,17 +1,38 @@
-import { AppUserLayout } from "@eden/package-ui";
+import { AppUserLayout, Button } from "@eden/package-ui";
+import Image from "next/image";
 import { useRouter } from "next/router";
-import { useState } from "react";
 
 import { NextPageWithLayout } from "../_app";
 
 const RequestAccess: NextPageWithLayout = () => {
-  const router = useRouter();
-
   return (
     <>
-      <div className="flex h-screen w-full items-center justify-center"></div>
+      <div className="mb-1 flex h-screen w-full flex-col items-center justify-center space-y-2">
+        <Image
+          className=" mt-16"
+          alt="sleeping dashboard'
+"
+          src="/sleeping-dashboard.png"
+          width={216}
+          height={83}
+        ></Image>
+
+        <p className="my-4 font-bold">You do not have access to this page!</p>
+
+        <Button
+          variant="primary"
+          className="font-Unica  mt-4 h-12  w-36  border-neutral-400 font-medium text-black "
+          onClick={() => {
+            console.log("");
+          }}
+        >
+          <p className="text-sm">Request Access</p>
+        </Button>
+      </div>
     </>
   );
 };
+
+RequestAccess.getLayout = (page: any) => <AppUserLayout>{page}</AppUserLayout>;
 
 export default RequestAccess;
