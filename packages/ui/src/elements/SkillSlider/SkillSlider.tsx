cf13a5fb86bdd54518cb230fe559165dd50539e7
Sergey Belyaev <79952005+sbelka-1703@users.noreply.github.com>
2023-09-26 15:09:51 +0200

    the score slider is now daynamic 1-10

--- packages/ui/src/elements/SkillSlider/SkillSlider.tsx
+++ packages/ui/src/elements/SkillSlider/SkillSlider.tsx
@@ -1,11 +1,23 @@
-export interface SkillSliderProps {}
+import clsx from "clsx";
 
-export const SkillSlider = ({}: SkillSliderProps) => {
+export interface SkillSliderProps {
+  name: string; // Since you are not using `name` inside the component, you can make it optional.
+  score: number;
+}
+
+export const SkillSlider = ({ name, score }: SkillSliderProps) => {
+  score = 2;
+  //The score is 1-10. I am converting the score to % with an additional "0". 1 = 10%, 2 = 20% .... 10 = 100%
+  const btnClasses = clsx(
+    "absolute top-1/2 h-5 w-5 transform -translate-x-1/2 -translate-y-1/2 rounded-full bg-blue-500",
+    `left-${score}0%`
+  );
+
   return (
     <div className="relative flex items-center space-x-2">
-      <h3>Proficient in other front-end frameworks</h3>
+      <h3>{name || "Proficient in other front-end frameworks"}</h3> d{" "}
       <div className="relative h-[5px] w-52 rounded-lg bg-gray-600">
-        <div className="absolute left-1/2 top-1/2 h-5 w-5 -translate-x-1/2 -translate-y-1/2 transform rounded-full bg-blue-500"></div>
+        <div className={btnClasses}></div>
       </div>
     </div>
   );
