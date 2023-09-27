e899150b186b641dbead2a1598e9949cc172a923
heavensbee <heavensbee001@gmail.com>
2023-09-27 13:26:53 +0200

    Add menu + logout btn to nav bar

--- packages/ui/src/components/MenuDropdown/MenuDropdown.tsx
+++ packages/ui/src/components/MenuDropdown/MenuDropdown.tsx
@@ -1,29 +1,23 @@
 import { Fragment, useState } from "react";
 import { BsThreeDots } from "react-icons/bs";
-import { classNames } from "../../../utils";
-import {
-  EdenIconQuestion,
-  EdenIconExclamationAndQuestion,
-} from "../../elements";
-import { InterviewEdenAI } from "../InterviewEdenAI";
-import { Transition } from "@headlessui/react";
-import { children } from "cheerio/lib/api/traversing";
 
-enum Platforms {
-  "twitter",
-  "discord",
-  "github",
-  "notion",
-  "telegram",
-  "linkedin",
-}
+import { classNames } from "../../../utils";
 
 export interface IMenuDropdownProps {
   children: Array<React.ReactElement>;
+  clickableElement?: React.ReactElement;
+  positionY?: "top" | "bottom";
+  positionX?: "left" | "right";
 }
 
-export const MenuDropdown = ({ children }: IMenuDropdownProps) => {
+export const MenuDropdown = ({
+  children,
+  clickableElement = <BsThreeDots />,
+  positionY = "bottom",
+  positionX = "left",
+}: IMenuDropdownProps) => {
   const [open, setOpen] = useState<boolean>(false);
+
   return (
     <>
       <div>
@@ -31,19 +25,20 @@ export const MenuDropdown = ({ children }: IMenuDropdownProps) => {
           className="hover:bg-edenGray-100 rounded-full w-8 h-8 flex items-center justify-center cursor-pointer"
           onClick={() => setOpen(!open)}
         >
-          <BsThreeDots />
+          {clickableElement}
         </div>
 
         <ul
           className={classNames(
-            "z-40 rounded-sm style-none border-box bg-white border-edenGray-100 w-[50vh] absolute right-0 top-8 overflow-hidden transition-all duration-200 ease-in-out",
-            open ? "max-h-[50vh] border" : "max-h-0"
+            "z-40 rounded-sm style-none border-box bg-white border-edenGray-100 w-[50vh] absolute right-0 overflow-hidden transition-all duration-200 ease-in-out",
+            open ? "max-h-[50vh] border" : "max-h-0",
+            positionY === "top" ? "bottom-8" : "top-8",
+            positionX === "right" ? "!left-0" : ""
           )}
           style={{ maxWidth: "15rem" }}
           onClick={() => setOpen(false)}
         >
-          {children &&
-            children.map((item: React.ReactElement, index: number) => item)}
+          {children && children.map((item: React.ReactElement) => item)}
         </ul>
       </div>
       {open && (
