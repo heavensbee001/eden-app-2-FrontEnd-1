da7a6e25349c33ad83663777f202277cf08f2a48
Sergey Belyaev <79952005+sbelka-1703@users.noreply.github.com>
2023-09-29 12:52:26 +0200

    corrected build error

--- packages/ui/src/elements/SkillSlider/SkillSlider.tsx
+++ packages/ui/src/elements/SkillSlider/SkillSlider.tsx
@@ -1,5 +1,4 @@
-import { EdenTooltip } from "../../components";
-import { EdenIconExclamation } from "../EdenIcons";
+// import { EdenTooltip } from "../../components";
 
 export interface Candidate {
   name: string;
@@ -13,12 +12,7 @@ export interface SkillSliderProps {
 export const SkillSlider = ({ name, score }: Candidate) => {
   return (
     <div className="relative flex items-center justify-between space-x-2">
-      <EdenTooltip>
-        {" "}
-        {/* <div className="bg-edenPink-200 h-5 w-5 rounded-full p-1 shadow">
-          <EdenIconExclamation className="h-full w-full" />
-        </div> */}
-      </EdenTooltip>
+      {/* <EdenTooltip></EdenTooltip> */}
       <p className="w-9/12">{name}</p>
       <div className="relative mt-1 h-[5px] w-52 rounded-lg bg-gray-300">
         <div
