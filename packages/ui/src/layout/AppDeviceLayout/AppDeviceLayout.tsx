ac6e2310ab825d84e560b784651f3d2c2b8897a9
heavensbee <heavensbee001@gmail.com>
2023-10-04 09:44:41 +0200

    Mobile device block

--- packages/ui/src/layout/AppDeviceLayout/AppDeviceLayout.tsx
+++ packages/ui/src/layout/AppDeviceLayout/AppDeviceLayout.tsx
@@ -1,8 +1,12 @@
 import { MdMobileOff } from "react-icons/md";
 
-interface IAppDeviceLayoutProps {}
+interface IAppDeviceLayoutProps {
+  children: any;
+}
 
-export const AppDeviceLayout = ({}: IAppDeviceLayoutProps) => {
+export const AppDeviceLayout = ({ children }: IAppDeviceLayoutProps) => {
+  console.log(children);
+
   const UA = navigator.userAgent;
   const isMobile = Boolean(
     UA.match(
@@ -10,23 +14,25 @@ export const AppDeviceLayout = ({}: IAppDeviceLayoutProps) => {
     )
   );
 
-  return (
-    <>
-      {isMobile && (
-        <section className="fixed top-0 left-0 z-50 flex h-screen w-screen flex-col items-center justify-center bg-white">
-          <p className="text-darkGreen -ml-2 mb-4 text-2xl font-bold">
-            🌱 Eden.
-          </p>
-          <p className="mb-2 text-center">
-            Mobile site is
-            <br /> under construction.
-            <br /> Use your laptop.
-          </p>
-          <MdMobileOff className="text-2xl" />
-        </section>
-      )}
-    </>
-  );
+  if (isMobile) {
+    return (
+      <>
+        {isMobile && (
+          <section className="fixed top-0 left-0 z-50 flex h-screen w-screen flex-col items-center justify-center bg-white">
+            <h1 className="text-edenGreen-600">Eden</h1>
+            <p className="mb-2 text-center">
+              Mobile site is
+              <br /> under construction.
+              <br /> Use your laptop.
+            </p>
+            <MdMobileOff className="text-2xl" />
+          </section>
+        )}
+      </>
+    );
+  } else {
+    return children;
+  }
 };
 
 export default AppDeviceLayout;
