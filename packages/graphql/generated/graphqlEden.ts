295a301ed14f714cdbde1b7fdbf5e5a95888739e
Miltiadis Saratzidis <miltossaratzidis1@gmail.com>
2023-10-24 15:34:39 -0400

    averageScore done

--- packages/graphql/generated/graphqlEden.ts
+++ packages/graphql/generated/graphqlEden.ts
@@ -3,10 +3,12 @@ export type InputMaybe<T> = Maybe<T>;
 export type Exact<T extends { [key: string]: unknown }> = {
   [K in keyof T]: T[K];
 };
-export type MakeOptional<T, K extends keyof T> = Omit<T, K> &
-  { [SubKey in K]?: Maybe<T[SubKey]> };
-export type MakeMaybe<T, K extends keyof T> = Omit<T, K> &
-  { [SubKey in K]: Maybe<T[SubKey]> };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & {
+  [SubKey in K]?: Maybe<T[SubKey]>;
+};
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & {
+  [SubKey in K]: Maybe<T[SubKey]>;
+};
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
   ID: string;
@@ -4744,12 +4746,13 @@ export type ScoreCardMemoryInput = {
 export type ScoreCardTotalType = {
   __typename?: "scoreCardTotalType";
   score?: Maybe<Scalars["Float"]>;
-  scoreCardsCalculated?: Maybe<Scalars["Boolean"]>;
+  scoreCardCalculated?: Maybe<Scalars["Boolean"]>;
 };
 
 export type ScoreCardsCandidateType = {
   __typename?: "scoreCardsCandidateType";
   card?: Maybe<CardMemory>;
+  scoreAlignment?: Maybe<Scalars["Float"]>;
 };
 
 export type ScoreCardsPositionType = {
