b5cab6dd9d27edb59d752d121dab46781f421589
Sergey Belyaev <79952005+sbelka-1703@users.noreply.github.com>
2023-11-28 08:52:13 -0700

    all section poen by default

--- apps/saas/components/interview/ApplicationContainer.tsx
+++ apps/saas/components/interview/ApplicationContainer.tsx
@@ -78,8 +78,8 @@ const ApplicationStepContainer = ({
     strongSuit: boolean;
   }>({
     areasToImprove: true,
-    growth: false,
-    strongSuit: false,
+    growth: true,
+    strongSuit: true,
   });
 
   const formattedSalary = (salary: number) => {
@@ -104,7 +104,7 @@ const ApplicationStepContainer = ({
               onClick={() =>
                 setOpenSections({
                   ...openSections,
-                  areasToImprove: openSections.areasToImprove,
+                  areasToImprove: !openSections.areasToImprove,
                 })
               }
             >
