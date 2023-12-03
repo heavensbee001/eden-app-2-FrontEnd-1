8dc8122ba8fdea3bececc77d0ba4c2a7c3e9bd1c
Sergey Belyaev <79952005+sbelka-1703@users.noreply.github.com>
2023-12-02 18:37:27 -0700

    removed og from document

--- apps/saas/pages/_document.tsx
+++ apps/saas/pages/_document.tsx
@@ -29,17 +29,6 @@ class MyDocument extends Document {
           <link rel="apple-touch-icon" sizes="192x192" href="/logo192.png" />
           <link rel="icon" type="image/png" sizes="32x32" href="/logo32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/logo16.png" />
-          <meta property="og:url" content={appUrl} />
-          <meta property="og:site_name" content={`Eden protocol`} />
-          <meta property="og:title" content={title} />
-          <meta property="og:description" content={description} />
-          <meta
-            property="og:image"
-            content={`https://pbs.twimg.com/profile_images/1563942271170617344/4Tpfr8SY_400x400.jpg`}
-          />
-          <meta property="og:image:width" content="400" />
-          <meta property="og:image:height" content="400" />
-
           <meta property="twitter:card" content="summary" />
           <meta property="twitter:site" content={`Eden protocol`} />
           <meta property="twitter:title" content={title} />
