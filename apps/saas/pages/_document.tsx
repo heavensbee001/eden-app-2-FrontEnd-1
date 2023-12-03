4d8dc00491dc98e77c8f3636fe3725d362c044e0
Sergey Belyaev <79952005+sbelka-1703@users.noreply.github.com>
2023-12-02 18:43:49 -0700

    modified meta tags

--- apps/saas/pages/_document.tsx
+++ apps/saas/pages/_document.tsx
@@ -29,26 +29,27 @@ class MyDocument extends Document {
           <link rel="apple-touch-icon" sizes="192x192" href="/logo192.png" />
           <link rel="icon" type="image/png" sizes="32x32" href="/logo32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/logo16.png" />
-          <meta content={appUrl} />
-          <meta content={`Eden protocol`} />
-          <meta content={title} />
-          <meta content={description} />
-          <meta
-            content={`https://pbs.twimg.com/profile_images/1563942271170617344/4Tpfr8SY_400x400.jpg`}
-          />
-          <meta content="400" />
-          <meta content="400" />
+          <meta name="description" content={description} />
 
-          <meta content="summary" />
-          <meta content={`Eden protocol`} />
-          <meta content={title} />
-          <meta content={description} />
+          {/* Open Graph / Facebook Meta Tags */}
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content={appUrl} />
+          <meta property="og:title" content={title} />
+          <meta property="og:description" content={description} />
           <meta
-            content={`https://pbs.twimg.com/profile_images/1563942271170617344/4Tpfr8SY_400x400.jpg`}
+            property="og:image"
+            content="https://pbs.twimg.com/profile_images/1563942271170617344/4Tpfr8SY_400x400.jpg"
           />
-          <meta content="400" />
-          <meta content="400" />
-          <meta content={`Eden protocol`} />
+
+          {/* Twitter Meta Tags */}
+          <meta property="twitter:card" content="summary_large_image" />
+          <meta property="twitter:url" content={appUrl} />
+          <meta property="twitter:title" content={title} />
+          <meta property="twitter:description" content={description} />
+          <meta
+            property="twitter:image"
+            content="https://pbs.twimg.com/profile_images/1563942271170617344/4Tpfr8SY_400x400.jpg"
+          />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link
             rel="preconnect"
