339b10b0e759fd1428143478b3ec3f36ef748f9e
Sergey Belyaev <79952005+sbelka-1703@users.noreply.github.com>
2023-12-02 18:39:57 -0700

    fix

--- apps/saas/pages/_document.tsx
+++ apps/saas/pages/_document.tsx
@@ -29,17 +29,26 @@ class MyDocument extends Document {
           <link rel="apple-touch-icon" sizes="192x192" href="/logo192.png" />
           <link rel="icon" type="image/png" sizes="32x32" href="/logo32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/logo16.png" />
-          <meta property="twitter:card" content="summary" />
-          <meta property="twitter:site" content={`Eden protocol`} />
-          <meta property="twitter:title" content={title} />
-          <meta property="twitter:description" content={description} />
+          <meta content={appUrl} />
+          <meta content={`Eden protocol`} />
+          <meta content={title} />
+          <meta content={description} />
           <meta
-            property="twitter:image:src"
             content={`https://pbs.twimg.com/profile_images/1563942271170617344/4Tpfr8SY_400x400.jpg`}
           />
-          <meta property="twitter:image:width" content="400" />
-          <meta property="twitter:image:height" content="400" />
-          <meta property="twitter:creator" content={`Eden protocol`} />
+          <meta content="400" />
+          <meta content="400" />
+
+          <meta content="summary" />
+          <meta content={`Eden protocol`} />
+          <meta content={title} />
+          <meta content={description} />
+          <meta
+            content={`https://pbs.twimg.com/profile_images/1563942271170617344/4Tpfr8SY_400x400.jpg`}
+          />
+          <meta content="400" />
+          <meta content="400" />
+          <meta content={`Eden protocol`} />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link
             rel="preconnect"
