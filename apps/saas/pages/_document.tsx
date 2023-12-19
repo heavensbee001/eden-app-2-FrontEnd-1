1ad275248bad9f5b2161fe5521ff60894a2228ea
heavensbee <heavensbee001@gmail.com>
2023-12-19 09:25:31 +0100

    Try fix

--- apps/saas/pages/_document.tsx
+++ apps/saas/pages/_document.tsx
@@ -6,15 +6,6 @@ import Document, {
   NextScript,
 } from "next/document";
 
-const appUrl =
-  process.env.NEXT_PUBLIC_ENV_BRANCH === "develop"
-    ? `https://eden-foundation-develop.vercel.app/`
-    : `https://edenprotocol.app/`;
-const title = process.env.NEXT_PUBLIC_ENV_BRANCH
-  ? `Eden protocol - ${process.env.NEXT_PUBLIC_ENV_BRANCH}`
-  : `Eden protocol`;
-const description = `Together, let's build the perfect breeding ground for everyone to do work they love. Eden's talent coordination protocol is how.`;
-
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const initialProps = await Document.getInitialProps(ctx);
@@ -29,27 +20,7 @@ class MyDocument extends Document {
           <link rel="apple-touch-icon" sizes="192x192" href="/logo192.png" />
           <link rel="icon" type="image/png" sizes="32x32" href="/logo32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/logo16.png" />
-          <meta name="description" content={description} />
 
-          {/* Open Graph / Facebook Meta Tags */}
-          <meta property="og:type" content="website" />
-          <meta property="og:url" content={appUrl} />
-          <meta property="og:title" content={title} />
-          <meta property="og:description" content={description} />
-          <meta
-            property="og:image"
-            content="https://pbs.twimg.com/profile_images/1563942271170617344/4Tpfr8SY_400x400.jpg"
-          />
-
-          {/* Twitter Meta Tags */}
-          <meta property="twitter:card" content="summary_large_image" />
-          <meta property="twitter:url" content={appUrl} />
-          <meta property="twitter:title" content={title} />
-          <meta property="twitter:description" content={description} />
-          <meta
-            property="twitter:image"
-            content="https://pbs.twimg.com/profile_images/1563942271170617344/4Tpfr8SY_400x400.jpg"
-          />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link
             rel="preconnect"
