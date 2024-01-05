25c86f6ed75c0b9c4630cb7826a387848a60d6d6
heavensbee <heavensbee001@gmail.com>
2024-01-05 13:53:41 +0100

    Add office location input placeholder

--- apps/saas/pages/[slug]/jobs/[positionID].tsx
+++ apps/saas/pages/[slug]/jobs/[positionID].tsx
@@ -555,6 +555,7 @@ const PositionPage: NextPageWithLayout = ({
                     <div className="bg-edenGreen-600 text-edenPink-300 font-Moret mr-2 inline-block rounded-xl px-3 py-0.5 font-bold">
                       <input
                         {...register("generalDetails.officeLocation")}
+                        placeholder="Office location"
                         className={classNames(editInputClasses, "")}
                       />
                     </div>
