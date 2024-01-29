e73745be8fe892069c5f0d02ca4473ae3f0e7ca7
heavensbee <heavensbee001@gmail.com>
2024-01-29 09:45:26 +0100

    Jobs page seo

---
+++ apps/saas/pages/api/fc/next-job.ts
@@ -0,0 +1,132 @@
+/* eslint-disable import/no-anonymous-default-export */
+import { Position } from "@eden/package-graphql/generated";
+import axios from "axios";
+import { NextApiRequest, NextApiResponse } from "next";
+
+const baseUrl =
+  process.env.NEXT_PUBLIC_ENV_BRANCH === "develop"
+    ? `https://eden-saas-staging.vercel.app`
+    : `https://edenprotocol.app`;
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === "POST") {
+    try {
+      const currentJobId = (req.query["job"] as string) || "";
+      const community = (req.query["community"] as string) || "";
+
+      if (!community) {
+        return res.status(400).send("Missing community");
+      }
+
+      const communityPositions = await axios.post(
+        process.env.NEXT_PUBLIC_GRAPHQL_URL as string,
+        {
+          headers: {
+            "Access-Control-Allow-Origin": `*`,
+          },
+          variables: {
+            fields: { slug: community },
+          },
+          query: `
+            query Query($fields: findPositionsOfCommunityInput) {
+              findPositionsOfCommunity(fields: $fields) {
+                _id
+                name
+                status
+                icon
+                generalDetails {
+                  officePolicy
+                  contractType
+                  yearlySalary {
+                    min
+                    max
+                  }
+                }
+                company {
+                  _id
+                  name
+                  slug
+                  imageUrl
+                  whatsToLove
+                }
+              }
+            }
+          `,
+        }
+      );
+
+      const _positions =
+        communityPositions.data.data.findPositionsOfCommunity.filter(
+          (_position: Position) =>
+            _position?.status !== "ARCHIVED" &&
+            _position?.status !== "UNPUBLISHED" &&
+            _position?.status !== "DELETED"
+        ) as Position[];
+
+      const positionIndex = _positions.findIndex(
+        (_pos: Position) => _pos._id === currentJobId
+      );
+
+      const newPosition = _positions[positionIndex + 1];
+
+      const imageUrl = getImageUrl(newPosition);
+
+      // Return an HTML response
+      res.setHeader("Content-Type", "text/html");
+      res.status(200).send(`
+        <!DOCTYPE html>
+        <html>
+          <head>
+            <title>Eden Jobs</title>
+            <meta property="og:title" content="Eden Jobs">
+            <meta property="og:image" content="${imageUrl}">
+            <meta name="fc:frame" content="vNext">
+            <meta name="fc:frame:image" content="${imageUrl}">
+            <meta name="fc:frame:post_url" content="${baseUrl}/api/next-job?job=${newPosition._id}&community=${community}">
+            <meta name="fc:frame:button:1" content="See next job">
+          </head>
+          <body>
+          </body>
+        </html>
+      `);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send("Error generating image");
+    }
+  } else {
+    // Handle any non-POST requests
+    res.setHeader("Allow", ["POST"]);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+};
+
+function getImageUrl(position: Position) {
+  const DEFAULT_IMAGE = `https://pbs.twimg.com/profile_images/1595723986524045312/fqOO4ZI__400x400.jpg`;
+
+  const imageSrc = position.company?.imageUrl
+    ? position.company?.imageUrl
+    : DEFAULT_IMAGE;
+
+  let imageUrl = `${baseUrl}/api/og/position?image=${imageSrc}`;
+
+  if (position.name) {
+    imageUrl = imageUrl + `&position=${position.name}`;
+  }
+  if (position.generalDetails?.yearlySalary?.min) {
+    imageUrl =
+      imageUrl + `&salaryMin=${position.generalDetails?.yearlySalary?.min}`;
+  }
+  if (position.generalDetails?.yearlySalary?.max) {
+    imageUrl =
+      imageUrl + `&salaryMax=${position.generalDetails?.yearlySalary?.max}`;
+  }
+  if (position.generalDetails?.officePolicy) {
+    imageUrl =
+      imageUrl + `&officePolicy=${position.generalDetails?.officePolicy}`;
+  }
+  if (position.generalDetails?.officeLocation) {
+    imageUrl = imageUrl + `&location=${position.generalDetails.officeLocation}`;
+  }
+
+  return imageUrl;
+}
