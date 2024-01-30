c5c7754e915f0400fe21c65e9c69dbbcc41ce391
heavensbee <heavensbee001@gmail.com>
2024-01-30 08:50:30 +0100

    Fix typescript and session providers

--- packages/ui/src/layout/AppUserLayoutNew/AppUserLayout.stories.tsx
+++ packages/ui/src/layout/AppUserLayoutNew/AppUserLayout.stories.tsx
@@ -1,5 +1,5 @@
+import { DynamicSessionProvider } from "@eden/package-context";
 import { ComponentMeta, ComponentStory } from "@storybook/react";
-import { SessionProvider } from "next-auth/react";
 
 import { AppUserLayoutNew } from "./AppUserLayoutNew";
 
@@ -10,20 +10,9 @@ export default {
 } as ComponentMeta<typeof AppUserLayoutNew>;
 
 const Template: ComponentStory<typeof AppUserLayoutNew> = (args) => (
-  <SessionProvider
-    session={{
-      expires: "1",
-      user: {
-        id: "1",
-        email: "a",
-        name: "Miral",
-        image:
-          "https://pbs.twimg.com/profile_images/1513838045589430277/4Pxad6DL_400x400.jpg",
-      },
-    }}
-  >
+  <DynamicSessionProvider fetchingToken={false}>
     <AppUserLayoutNew {...args} />
-  </SessionProvider>
+  </DynamicSessionProvider>
 );
 
 export const Default = Template.bind({});
