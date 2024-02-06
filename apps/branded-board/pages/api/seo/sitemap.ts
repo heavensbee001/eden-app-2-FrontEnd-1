0a50b54ea96fc23a65873fa70c30ecae983ab5a5
heavensbee <heavensbee001@gmail.com>
2024-02-06 15:48:20 +0100

    Add all job pages to subdomain sitemap

--- apps/branded-board/pages/api/seo/sitemap.ts
+++ apps/branded-board/pages/api/seo/sitemap.ts
@@ -1,11 +1,51 @@
-// pages/api/sitemap.xml.js
-
+import axios from "axios";
+import { log } from "console";
 import { NextApiRequest, NextApiResponse } from "next";
 import { SitemapStream, streamToPromise } from "sitemap";
 
+type Url = {
+  url: string;
+  changefreq: string;
+  priority: number;
+};
+
 const getSubdomainUrls = async (subdomain: string) => {
   // Fetch subdomain URLs from the database
-  return ["/jobs"];
+  const companyRes = await axios.post(
+    process.env.NEXT_PUBLIC_GRAPHQL_URL as string,
+    {
+      headers: {
+        "Access-Control-Allow-Origin": `*`,
+      },
+      variables: { fields: { slug: subdomain } },
+      query: `
+        query ($fields: findCompanyFromSlugInput) {
+          findCompanyFromSlug(fields: $fields) {
+            positions {
+              _id
+            }
+          }
+        }
+      `,
+    }
+  );
+
+  const _positionIds =
+    companyRes?.data?.data?.findCompanyFromSlug?.positions?.map(
+      (position: { _id: string }) => position._id
+    ) || [];
+
+  const positionRoutes: Url[] = _positionIds.map((id: string) => ({
+    url: `/jobs/${id}`,
+    changefreq: "daily",
+    priority: 0.7,
+  }));
+
+  log("positionRoutes", positionRoutes);
+  return [
+    { url: "/jobs", changefreq: "daily", priority: 1 },
+    ...positionRoutes,
+  ];
 };
 
 export default async function handler(
@@ -14,7 +54,12 @@ export default async function handler(
 ) {
   try {
     const subdomain =
-      process.env.NEXT_PUBLIC_FORCE_SLUG_LOCALHOST || req.query.subdomain;
+      process.env.NEXT_PUBLIC_FORCE_SLUG_LOCALHOST ||
+      (req.query.subdomain as string);
+
+    if (!subdomain) {
+      return res.status(400).json({ error: "Subdomain is required" });
+    }
 
     // Get subdomain URLs
     const subdomainUrls = await getSubdomainUrls(subdomain);
@@ -25,8 +70,12 @@ export default async function handler(
     });
 
     // Add URLs to the sitemap stream
-    subdomainUrls.forEach((url) => {
-      stream.write({ url, changefreq: "daily", priority: 0.7 });
+    subdomainUrls.forEach((_url) => {
+      stream.write({
+        url: _url.url,
+        changefreq: _url.changefreq,
+        priority: _url.priority,
+      });
     });
 
     // End sitemap stream
